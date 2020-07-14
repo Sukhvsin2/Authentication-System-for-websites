1b1a1bceeedbf0a4c47cf6cc9d10fef694d7a546
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-07-14 15:10:09 +0530

    Secret key variable removed

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt,{secret: secret, encryptedFields: ['password'] });
+
+userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("user", userSchema);
 
